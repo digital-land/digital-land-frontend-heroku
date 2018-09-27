862ee807d8fe5c06915b450ca92d977949c5ea53
Colm Britton <colmjude@gmail.com>
2018-09-27 12:42:24 +0800

    Added gulp to build stylesheets

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ Digital Land Frontend
 Digital Land Frontend contains the code used to build user interfaces for the projects that MHCLG's digital land team are working on.
 
 It builds on the [GOVUK Design System](https://github.com/alphagov/govuk-frontend) code, making the majority of it work with Flask applications.
+
+Requirements
+
+- [Python 3](https://www.python.org/)
+- [Node](https://nodejs.org/en/) and [Npm](https://www.npmjs.com/)
+- [Gulp](https://gulpjs.com/)
+
+Getting Started
+===============
+
+Install front end build tool (gulp)
+
+    npm install && gulp scss
+
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const gulp = require("gulp"),
+      sass = require("gulp-sass"),
+      clean = require('gulp-clean');
+
+// set paths ...
+const config = {
+	scssPath: "src/scss",
+	destPath: "application/static/stylesheets",
+  govukAssetPath: "application/static/govuk-frontend/assets"
+}
+
+// Delete our old stylesheets files
+gulp.task('clean-css', function () {
+  return gulp.src('application/static/stylesheets/**/*', {read: false})
+    .pipe(clean());
+});
+
+// compile scss to CSS
+gulp.task("scss", ['clean-css', 'copy-assets'], function() {
+	return gulp.src( config.scssPath + '/*.scss')
+	.pipe(sass({outputStyle: 'expanded',
+		includePaths: [ 'src/scss',
+			'src/govuk-frontend']})).on('error', sass.logError)
+	.pipe(gulp.dest(config.destPath))
+})
+
+// Watch src folder for changes
+gulp.task("watch", ["scss"], function () {
+  gulp.watch("src/scss/**/*", ["scss"])
+});
+
+gulp.task('copy-assets', function() {
+  gulp.src('src/govuk-frontend/assets/**/*')
+    .pipe(gulp.dest(config.govukAssetPath));
+});
+
+// Set watch as default task
+gulp.task("default", ["watch"]);
