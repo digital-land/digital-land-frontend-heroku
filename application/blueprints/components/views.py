66adca0cdbb7056d196cd9f52417c3000353c604
Colm Britton <colmjude@gmail.com>
2020-05-04 16:43:04 +0100

    Move dl-components to own directory

--- application/blueprints/components/views.py
+++ application/blueprints/components/views.py
@@ -6,49 +6,51 @@ from flask import (
 
 components = Blueprint('components', __name__, template_folder='templates', url_prefix='/component')
 
+dl_component_template_dir = 'dl-component-examples/'
+
 @components.route('/buttons')
 def buttons():
-    return render_template('buttons.html')
+    return render_template(f'{dl_component_template_dir}buttons.html')
 
 @components.route('/contribution-bars')
 def contribution_bars():
-    return render_template('contribution-bars.html')
+    return render_template(f'{dl_component_template_dir}contribution-bars.html')
 
 @components.route('/data-tables')
 def data_tables():
-    return render_template('data-tables.html')
+    return render_template(f'{dl_component_template_dir}data-tables.html')
 
 @components.route('/filter-group')
 def filter_group():
-    return render_template('filter-group.html')
+    return render_template(f'{dl_component_template_dir}filter-group.html')
 
 @components.route('/hero')
 def hero():
-    return render_template('hero.html')
+    return render_template(f'{dl_component_template_dir}hero/index.html')
 
 @components.route('/hero/example')
 def hero_example():
-    return render_template('hero-example.html')
+    return render_template(f'{dl_component_template_dir}hero/example.html')
 
 @components.route('/highlight-box')
 def highlight_box():
-    return render_template('highlight-box.html')
+    return render_template(f'{dl_component_template_dir}highlight-box.html')
 
 @components.route('/hub')
 def hub():
-    return render_template('hub.html')
+    return render_template(f'{dl_component_template_dir}hub.html')
 
 @components.route('/info-text')
 def info_text():
-    return render_template('info-text.html')
+    return render_template(f'{dl_component_template_dir}info-text.html')
 
 @components.route('/contents-section')
 def contents_section():
-    return render_template('contents.html')
+    return render_template(f'{dl_component_template_dir}contents.html')
 
 @components.route('/lists')
 def lists():
-    return render_template('lists.html')
+    return render_template(f'{dl_component_template_dir}lists.html')
 
 @components.route('/lists/organisation-example')
 def organisation_list_example():
@@ -64,31 +66,31 @@ def pagination():
 
 @components.route('/panels')
 def panels():
-    return render_template('panels.html')
+    return render_template(f'{dl_component_template_dir}panels.html')
 
 @components.route('/related-items')
 def related_items():
-    return render_template('related-items.html')
+    return render_template(f'{dl_component_template_dir}related-items.html')
 
 @components.route('/search')
 def search():
-    return render_template('search.html')
+    return render_template(f'{dl_component_template_dir}search.html')
 
 @components.route('/tabs')
 def tabs():
-    return render_template('tabs.html')
+    return render_template(f'{dl_component_template_dir}tabs.html')
 
 @components.route('/tags')
 def tags():
-    return render_template('tags.html')
+    return render_template(f'{dl_component_template_dir}tags.html')
 
 @components.route('/task-list')
 def task_list():
-    return render_template('task-list.html')
+    return render_template(f'{dl_component_template_dir}task-list.html')
 
 @components.route('/visualising-data')
 def visualising_data():
-    return render_template('visualising-data.html')
+    return render_template(f'{dl_component_template_dir}visualising-data.html')
 
 @components.route('/tags/example')
 def tags_example():
