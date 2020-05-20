938d2127379de6e3bba25ebc2d2486ba3342d08f
Colm Britton <colmjude@gmail.com>
2020-05-20 16:08:48 +0100

    Make linkable table js comply with standardjs

--- src/js/dl-components/linkable-table.js
+++ src/js/dl-components/linkable-table.js
@@ -1,130 +1,130 @@
-import "../../govuk/vendor/polyfills/Function/prototype/bind";
+import '../../govuk/vendor/polyfills/Function/prototype/bind'
 
-function LinkableTable($module) {
-  this.$module = $module;
+function LinkableTable ($module) {
+  this.$module = $module
 }
 
-LinkableTable.prototype.init = function(params) {
-  this.setupOptions(params);
+LinkableTable.prototype.init = function (params) {
+  this.setupOptions(params)
 
-  this.data_table = this.$module.querySelector("table");
-  this.data_table_head = this.data_table.querySelector("thead tr");
-  this.data_table_body = this.data_table.querySelector("tbody");
-  this.rows = this.data_table.querySelectorAll("tbody tr");
+  this.data_table = this.$module.querySelector('table')
+  this.data_table_head = this.data_table.querySelector('thead tr')
+  this.data_table_body = this.data_table.querySelector('tbody')
+  this.rows = this.data_table.querySelectorAll('tbody tr')
 
-  this.addLinkColumn();
+  this.addLinkColumn()
   // do I need to delay this?
-  this.initialSelected();
-};
+  this.initialSelected()
+}
 
-LinkableTable.prototype.setupOptions = function(params) {
-  params = params || {};
-  this.idPrefix = params.idPrefix || "table-x-";
-};
+LinkableTable.prototype.setupOptions = function (params) {
+  params = params || {}
+  this.idPrefix = params.idPrefix || 'table-x-'
+}
 
-LinkableTable.prototype.addLinkColumn = function() {
-  const first_cell = this.data_table_head.querySelector("th");
+LinkableTable.prototype.addLinkColumn = function () {
+  const firstCell = this.data_table_head.querySelector('th')
   this.data_table_head.insertBefore(
-    this.createCell({ type: "head" }),
-    first_cell
-  );
-  var rows = this.getTableRowsArray();
+    this.createCell({ type: 'head' }),
+    firstCell
+  )
+  var rows = this.getTableRowsArray()
   rows.forEach((row, idx) => {
-    console.log(idx);
-    idx = idx + 1;
-    var href = "#" + this.idPrefix + idx.toString();
+    console.log(idx)
+    idx = idx + 1
+    var href = '#' + this.idPrefix + idx.toString()
     if (row.id) {
-      href = "#" + row.id;
+      href = '#' + row.id
     }
     this.addCellToRow(
       row,
       this.createCell({ linkIdx: idx.toString(), linkHref: href })
-    );
-  });
-};
+    )
+  })
+}
 
-LinkableTable.prototype.createCell = function(params) {
-  params = params || {};
-  var boundLinkToRowHandler = this.linkToRowHandler.bind(this);
+LinkableTable.prototype.createCell = function (params) {
+  params = params || {}
+  var boundLinkToRowHandler = this.linkToRowHandler.bind(this)
   var cell =
-    params.type == "head"
-      ? document.createElement("th")
-      : document.createElement("td");
-  cell.classList.add("data-table__row-link");
+    params.type === 'head'
+      ? document.createElement('th')
+      : document.createElement('td')
+  cell.classList.add('data-table__row-link')
 
   if (params.linkIdx) {
-    var _link = document.createElement("a");
-    _link.setAttribute("href", params.linkHref);
-    _link.textContent = "#" + params.linkIdx;
-    _link.addEventListener("click", boundLinkToRowHandler);
-    console.log(_link);
-    cell.append(_link);
+    var _link = document.createElement('a')
+    _link.setAttribute('href', params.linkHref)
+    _link.textContent = '#' + params.linkIdx
+    _link.addEventListener('click', boundLinkToRowHandler)
+    console.log(_link)
+    cell.append(_link)
   }
-  return cell;
-};
+  return cell
+}
 
-LinkableTable.prototype.linkToRowHandler = function(e) {
-  //e.preventDefault();
-  this.deselectRows();
-  this.selectRow(e.currentTarget.closest("tr"));
-  console.log(e.currentTarget);
-};
+LinkableTable.prototype.linkToRowHandler = function (e) {
+  // e.preventDefault();
+  this.deselectRows()
+  this.selectRow(e.currentTarget.closest('tr'))
+  console.log(e.currentTarget)
+}
 
-LinkableTable.prototype.addCellToRow = function(row, cell) {
-  const first_cell = row.querySelector("td");
-  row.insertBefore(cell, first_cell);
-};
+LinkableTable.prototype.addCellToRow = function (row, cell) {
+  const firstCell = row.querySelector('td')
+  row.insertBefore(cell, firstCell)
+}
 
-LinkableTable.prototype.getTableRowsArray = function() {
-  var rows = [];
-  var trs = this.data_table_body.querySelectorAll("tr");
-  console.log(trs);
+LinkableTable.prototype.getTableRowsArray = function () {
+  var rows = []
+  var trs = this.data_table_body.querySelectorAll('tr')
+  console.log(trs)
   for (var i = 0; i < trs.length; i++) {
-    rows.push(trs[i]);
+    rows.push(trs[i])
   }
-  return rows;
-};
+  return rows
+}
 
-LinkableTable.prototype.deselectRows = function() {
+LinkableTable.prototype.deselectRows = function () {
   this.rows.forEach(row => {
-    row.classList.remove("data-table__row-selected");
-  });
-};
+    row.classList.remove('data-table__row-selected')
+  })
+}
 
-LinkableTable.prototype.selectRow = function(row) {
-  row.classList.add("data-table__row-selected");
-};
+LinkableTable.prototype.selectRow = function (row) {
+  row.classList.add('data-table__row-selected')
+}
 
-LinkableTable.prototype.highlightHashed = function(hash) {
-  var hashless_hash = hash.replace("#", "");
+LinkableTable.prototype.highlightHashed = function (hash) {
+  var hashlessHash = hash.replace('#', '')
 
   if (hash.includes(this.idPrefix)) {
-    const link = document.querySelector(`[href='${hash}']`);
-    const row = link.closest("tr");
-    this.selectRow(row);
-    row.scrollIntoView({ block: "center" });
-  } else if (document.getElementById(hashless_hash)) {
+    const link = document.querySelector(`[href='${hash}']`)
+    const row = link.closest('tr')
+    this.selectRow(row)
+    row.scrollIntoView({ block: 'center' })
+  } else if (document.getElementById(hashlessHash)) {
     // make sure element with id from hash is inside the table
-    var el = document.getElementById(hashless_hash);
+    var el = document.getElementById(hashlessHash)
     if (this.data_table_body.contains(el)) {
-      this.selectRow(el);
+      this.selectRow(el)
     }
   }
-};
+}
 
-LinkableTable.prototype.hashChangeHandler = function() {
-  this.deselectRows();
-  this.highlightHashed(window.location.hash);
-};
+LinkableTable.prototype.hashChangeHandler = function () {
+  this.deselectRows()
+  this.highlightHashed(window.location.hash)
+}
 
-LinkableTable.prototype.initialSelected = function() {
+LinkableTable.prototype.initialSelected = function () {
   if (window.location.hash) {
-    this.highlightHashed(window.location.hash);
+    this.highlightHashed(window.location.hash)
   }
 
   // add event listener for URL hash changes
-  const boundHashChangeHandler = this.hashChangeHandler.bind(this);
-  window.addEventListener("hashchange", boundHashChangeHandler);
-};
+  const boundHashChangeHandler = this.hashChangeHandler.bind(this)
+  window.addEventListener('hashchange', boundHashChangeHandler)
+}
 
-export default LinkableTable;
+export default LinkableTable
